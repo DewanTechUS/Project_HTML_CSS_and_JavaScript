b3bd6dd5eb051cac674a19505815a3c3f7a2b09c
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-25 13:35:16 -0500

    chore: add API config for IPify and geo.ipify services

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// https://ps-lms.vercel.app/curriculum/se/414/option-1
+// https://ps-lms.vercel.app/curriculum/se/414/option-1#:~:text=Scroll%20to%20top-,OPTION%201,IP%20Address%20Tracker,-Note
+// references to external APIs and libraries
+// https://www.youtube.com/watch?v=ly9vOqYFuMI
+// https://www.youtube.com/watch?v=Lq6umspUC5c
+// IP Geolocation API by IPify: https://geo.ipify.org/
+// LeafletJS map library:       https://leafletjs.com/
+// api URLs
+// IPify service to get our own IP address
+// this is my fevorite project ever/
+const IP_API_URL = "https://api.ipify.org?format=json";
+// IP Geolocation API endpoint
+const GEO_API_URL = "https://geo.ipify.org/api/v2/country,city";
+// api key from https://geo.ipify.org/
+const GEO_API_KEY = "https://geo.ipify.org/api/v2/country?apiKey=at_9ICOwxOKyrjKYENeqW14TfF5DPk2m&ipAddress=8.8.8.8";
